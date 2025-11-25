e131a87ba708c8e8b0c03e1cefd1fc569ea17037
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-11-25 22:04:03 +0700

    penambahan method

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,5 +9,7 @@ router.post('/login', authController.login);
 router.get('/all-users', jwtAuth, authController.getAllUsers);
 router.post('/reset-password', jwtAuth, authController.resetPassword);
 router.post('/update-role', jwtAuth, authController.updateRole);
+router.post('/update-username', jwtAuth, authController.updateUsername);
+router.post('/delete-account', jwtAuth, authController.deleteAccount);
 
 module.exports = router;
